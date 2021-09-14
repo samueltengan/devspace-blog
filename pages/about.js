3c73028ee6b7420d7444c30bdc7e01b00d321b2b
Samuel Tengan <tengansamuel@gmail.com>
2021-09-14 15:02:36 +0000

    About page warning fixed

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ const AboutPage = () => {
   return (
     <Layout>
       <h1 className="text-5xl border-b-4 pd-5 font-bold">About</h1>
-      <div className="bg-white shadow-md rounded-lg px-10 py-6 px-10 py-6 mt-6">
+      <div className="bg-white shadow-md rounded-lg px-10 py-6 mt-6">
         <h3 className="text-2xl mb-5">DevSpace Blog</h3>
         <p className="mb-3">This is a blog built with Next.js and Markdown</p>
 
