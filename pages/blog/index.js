f54feed392b2d8cabefd62b8c6baf8fe40f453aa
Samuel Tengan <tengansamuel@gmail.com>
2021-09-14 23:19:22 +0000

    Blog page updated with posts

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,11 +1,46 @@
 import Layout from "../../components/Layout";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 
-const Blogpage = () => {
+import Post from "../../components/Post";
+import { sortByDate } from "../../utils";
+
+export default function BlogPage({ posts }) {
   return (
     <Layout>
-      <h1>Blog</h1>
+      <h1 className="text-5xl border-b-4 p-5 font-bold">Blog</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => (
+          <Post post={post} key={index} />
+        ))}
+      </div>
     </Layout>
   );
-};
+}
 
-export default Blogpage;
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("posts"));
+  const posts = files.map((filename) => {
+    //remove .md from file names to create a slug
+    const slug = filename.replace(".md", "");
+
+    //Read data in files
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename), // file path
+      "utf-8"
+    );
+
+    //converting read .md files to json
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: { posts: posts.sort(sortByDate) },
+  };
+}
