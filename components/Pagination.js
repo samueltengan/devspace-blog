fc895b1bb19c925587ecb82c10fc82758fbd579c
Samuel Tengan <tengansamuel@gmail.com>
2021-09-15 12:07:46 +0000

    comments added

--- components/Pagination.js
+++ components/Pagination.js
@@ -13,6 +13,7 @@ const Pagination = ({ currentPage, numPages }) => {
     <div>
       <div className="mt-6">
         <ul className="flex pl-0 list-none my-2">
+          //Previous Button
           {!isFirst && (
             <Link href={prevPage}>
               <li className="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-800 mr-1 hover:bg-gray-200 cursor-pointer">
@@ -20,6 +21,7 @@ const Pagination = ({ currentPage, numPages }) => {
               </li>
             </Link>
           )}
+          //Adding Numbers
           {Array.from({ length: numPages }, (_, i) => (
             <Link href={`/blog/page/${i + 1}`}>
               <li className="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-800 mr-1 hover:bg-gray-200 cursor-pointer">
@@ -27,7 +29,7 @@ const Pagination = ({ currentPage, numPages }) => {
               </li>
             </Link>
           ))}
-
+          //Next Button
           {!isLast && (
             <Link href={nextPage}>
               <li className="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-800 mr-1 hover:bg-gray-200 cursor-pointer">
