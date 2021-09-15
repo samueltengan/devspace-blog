5a8a80a69d2d1415d6dcab3a807afcb3ff687623
Samuel Tengan <tengansamuel@gmail.com>
2021-09-15 11:18:38 +0000

    Pagination implemented

---
+++ pages/blog/page/[page_index].js
@@ -0,0 +1,77 @@
+import Layout from "../../../components/Layout";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+import Post from "../../../components/Post";
+
+import { POSTS_PER_PAGE } from "../../../config";
+
+export default function BlogPage({ posts, numPages, currentPage }) {
+  return (
+    <Layout>
+      <h1 className="text-5xl border-b-4 p-5 font-bold">Blog</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => (
+          <Post post={post} key={index} />
+        ))}
+      </div>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join("posts"));
+  const numPages = Math.ceil(files.length / POSTS_PER_PAGE);
+
+  let paths = [];
+
+  for (let i = 1; i <= numPages; i++) {
+    paths.push({
+      params: { page_index: i.toString() },
+    });
+  }
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const page = parseInt((params && params.page_index) || 1);
+  const files = fs.readdirSync(path.join("posts"));
+  const posts = files.map((filename) => {
+    //remove .md from file names to create a slug
+    const slug = filename.replace(".md", "");
+
+    //Read data in files
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename), // file path
+      "utf-8"
+    );
+
+    //converting read .md files to json
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  const numPages = Math.ceil(files.length / POSTS_PER_PAGE);
+  const pageIndex = page - 1;
+  const orderedPosts = posts.slice(
+    pageIndex * POSTS_PER_PAGE,
+    (pageIndex + 1) * POSTS_PER_PAGE
+  );
+
+  return {
+    props: {
+      posts: orderedPosts,
+      numPages,
+      currentPage: page,
+    },
+  };
+}
