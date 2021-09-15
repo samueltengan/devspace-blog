e643c1a464641197f8a96e2a5b9e6687c6ffebd1
Samuel Tengan <tengansamuel@gmail.com>
2021-09-15 11:18:54 +0000

    updated

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,46 +1,5 @@
-import Layout from "../../components/Layout";
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
+import BlogPage from "./page/[page_index]";
+import { getStaticProps } from "./page/[page_index]";
 
-import Post from "../../components/Post";
-import { sortByDate } from "../../utils";
-
-export default function BlogPage({ posts }) {
-  return (
-    <Layout>
-      <h1 className="text-5xl border-b-4 p-5 font-bold">Blog</h1>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {posts.map((post, index) => (
-          <Post post={post} key={index} />
-        ))}
-      </div>
-    </Layout>
-  );
-}
-
-export async function getStaticProps() {
-  const files = fs.readdirSync(path.join("posts"));
-  const posts = files.map((filename) => {
-    //remove .md from file names to create a slug
-    const slug = filename.replace(".md", "");
-
-    //Read data in files
-    const markdownWithMeta = fs.readFileSync(
-      path.join("posts", filename), // file path
-      "utf-8"
-    );
-
-    //converting read .md files to json
-    const { data: frontmatter } = matter(markdownWithMeta);
-
-    return {
-      slug,
-      frontmatter,
-    };
-  });
-
-  return {
-    props: { posts: posts.sort(sortByDate) },
-  };
-}
+export { getStaticProps };
+export default BlogPage;
